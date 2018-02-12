4759fe812c7b335e32a846ba663bf4aafdfd847b
jonnekleijer <jonne.kleijer@gmail.com>
2018-02-12 21:58:58 +0100

    Read data from local

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'cb5392c35661370d95f300086accea51/raw/'
-    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
-    'indicators.csv')
+df = pd.read_csv('data.csv')
 
 available_indicators = df['Indicator Name'].unique()
 
